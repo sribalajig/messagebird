4b287a8b338f405d1c3444be985f64da15022e9c
Balaji <balaji.gopinath@flaconi.de>
2017-10-22 10:29:15 +0200

    Added a test for SMS validation

---
+++ pkg/model/sms_test.go
@@ -0,0 +1,36 @@
+package model
+
+import "testing"
+
+var testCases = []struct {
+	SMS           SMS
+	IsValid       bool
+	ExpectedError string
+}{
+	{SMS{Recipient: "", Originator: "", Message: ""}, false, "recipient is empty,originator is empty,message is empty"},
+}
+
+func TestIsValid(t *testing.T) {
+	for _, testCase := range testCases {
+		isValid, err := testCase.SMS.IsValid()
+
+		if testCase.ExpectedError == "" && err != nil {
+			t.Logf("Expected no error but got error '%s'", err.Error())
+			t.Fail()
+		}
+
+		if testCase.ExpectedError != "" {
+			if err == nil {
+				t.Logf("Expected error '%s' but got no error", testCase.ExpectedError)
+				t.Fail()
+			} else {
+				t.Logf("Expected error '%s', got error '%s'", testCase.ExpectedError, err.Error())
+			}
+		}
+
+		if isValid != testCase.IsValid {
+			t.Logf("Expected %t, got %t", testCase.IsValid, isValid)
+			t.Fail()
+		}
+	}
+}
